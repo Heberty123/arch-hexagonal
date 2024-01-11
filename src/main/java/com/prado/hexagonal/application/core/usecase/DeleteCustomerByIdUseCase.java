634612445bfe72595ea2fd4a17cf63de3c0b2291
Heberty <hebertyprado22005@gmail.com>
2024-01-10 21:21:54 -0300

    adding usecase of delete

---
+++ src/main/java/com/prado/hexagonal/application/core/usecase/DeleteCustomerByIdUseCase.java
@@ -0,0 +1,23 @@
+package com.prado.hexagonal.application.core.usecase;
+
+import com.prado.hexagonal.application.ports.in.FindCustomerByIdInputPort;
+import com.prado.hexagonal.application.ports.out.DeleteCustomerByIdOutputPort;
+
+public class DeleteCustomerByIdUseCase {
+
+    private final FindCustomerByIdInputPort findCustomerByIdInputPort;
+
+    private final DeleteCustomerByIdOutputPort deleteCustomerByIdOutputPort;
+
+    public DeleteCustomerByIdUseCase(
+            FindCustomerByIdInputPort findCustomerByIdInputPort,
+            DeleteCustomerByIdOutputPort deleteCustomerByIdOutputPort) {
+        this.findCustomerByIdInputPort = findCustomerByIdInputPort;
+        this.deleteCustomerByIdOutputPort = deleteCustomerByIdOutputPort;
+    }
+
+    public void delete(String id) {
+        findCustomerByIdInputPort.find(id);
+        deleteCustomerByIdOutputPort.delete(id);
+    }
+}
